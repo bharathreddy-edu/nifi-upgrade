d01f1635645b4a4e72dc3cfbff0b5fdffcc8b6c5
bharathreddy-edu <bharathreddy-edu@ds.dtveng.net>
2022-11-16 13:43:38 -0800

    zookeeper installation check script

--- scripts/zookeeper-installation.sh
+++ scripts/zookeeper-installation.sh
@@ -3,9 +3,14 @@
 # environment variables file
 source ../sourcefile/env_variables.properties
 
+# Condition to check if zookeeper update is required or not
+if [[ ${ZK_UPDATE} ]]
+
+extractzkpid(){
 # zookeeper installation check, if zookeeper is present it run process on 2181 by default
-ZK_PID_EXIST=`dzdo netstat -plten | grep ${ZK_PORT} | awk '{print $9}' | awk -F / '{print $1}'`
-if [[ ! -z ${ZK_PID_EXIST} ]]; then
+ZK_PID_EXIST=`dzdo netstat -plten | grep ${ZK_PORT:=2181} | awk '{print $9}' | awk -F / '{print $1}'`
+if [[ ! -z ${ZK_PID_EXIST} ]];
+ then
     echo "Zookeeper is installed on this node";
     ZK_INSTALLED=true;
 else
@@ -13,14 +18,20 @@ else
     ZK_INSTALLED=false;
     exit 1;
 fi
+}
 
-
+killZookeeper(){
 # perform  steps to upgrade zookeeper
-if [[ ${ZK_INSTALLED} ]]; then
-  # Backing up Zookeeper directory
-  echo "Backing up Zookeeper"
-
+if [[ ${ZK_INSTALLED} ]];
+    then
+      # Kill the existing zookeeper
+      dzdo kill -9 ${ZK_PID_EXIST};
+    elif [[ ${?} -ne 0 ]];
+    then
+    service zookeeper stop;
 fi
+}
+
 
 
 
